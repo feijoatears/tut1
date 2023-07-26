d98d94ba0037efcf29f678c675fe500754ffe8b2
Jamie <jnicholsont3@gmail.com>
2023-07-26 20:06:03 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -1,26 +1,37 @@
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Greeter {
+public class Greeter 
+{
 
   private Locale locale;
   private String name;
 
-  public Greeter(String language, String country, String name) {
+  public Greeter(String language, String country, String name) 
+  {
     locale = new Locale(language, country);
     this.name = name;
   }
 
-  public String sayHello() {
+  public String sayHello() 
+  {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
     return messages.getString("greeting") + ", " + name;
   }
 
-  public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+  public static void main(String[] args) 
+  {
+    if(args.length == 3)
+    {
+        String language = args[0];
+        String country = args[1];
+        String name = args[2];
+        Greeter greeter = new Greeter(language, country, name);
+        System.out.println(greeter.sayHello());
+    }
+    else
+    {
+        System.out.println("Invalid number of arguments");
+    }
   }
 }
